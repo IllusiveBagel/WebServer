2e2c245c287d3e65e993d716c25790860f1226b2
IllusiveBagel <hawkentroop@gmail.com>
2020-09-15 09:13:25 +0100

    Added Basic Web Server Code

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace WebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Listen on port 51111, serving files in c:\webroot
+            var server = new WebServer("http://localhost:51111/", @"c:\webroot");
+
+            try
+            {
+                // Run the Web Server
+                server.Start();
+                Console.WriteLine("Server Running... Press Enter to Stop");
+                Console.ReadLine();
+            }
+            finally { server.Stop(); }
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,65 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServer
+{
+    class WebServer
+    {
+        HttpListener _listener;
+        string _baseFolder;     // Web Page Folder
+
+        public WebServer(string uriPrefix, string baseFolder)
+        {
+            _listener = new HttpListener();
+            _listener.Prefixes.Add(uriPrefix);
+            _baseFolder = baseFolder;
+        }
+
+        public async void Start()
+        {
+            _listener.Start();
+
+            while(true)
+            {
+                try
+                {
+                    var context = await _listener.GetContextAsync();
+                    Task.Run(() => ProcessRequestAsync(context));
+                }
+                catch(HttpListenerException)        { break; } // Listener Stopped
+                catch(InvalidOperationException)    { break; } // Listener Stopped
+            }
+        }
+
+        public void Stop() { _listener.Stop(); }
+
+        async void ProcessRequestAsync(HttpListenerContext context)
+        {
+            try
+            {
+                string filename = Path.GetFileName(context.Request.RawUrl);
+                string path = Path.Combine(_baseFolder, filename);
+                byte[] msg;
+
+                if(!File.Exists(path))
+                {
+                    Console.WriteLine($"Resource Not Found: {path}");
+                    context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                    msg = Encoding.UTF8.GetBytes("Sorry, that page does not exist");
+                }
+                else
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.OK;
+                    msg = File.ReadAllBytes(path);
+                }
+                context.Response.ContentLength64 = msg.Length;
+                using(Stream s = context.Response.OutputStream)
+                    await s.WriteAsync(msg, 0, msg.Length);
+            }
+            catch(Exception ex) { Console.WriteLine($"Request Error: {ex}"); }
+        }
+    }
+}
